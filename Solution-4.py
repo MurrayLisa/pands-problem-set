7815e1179568e25a37d943bbdb245e433340e72d
Lisa Murray <lisa.murray1605@gmail.com>
2019-03-18 14:58:44 +0000

    Solution 4 - draft 2

--- Solution-4.py
+++ Solution-4.py
@@ -1,9 +1,11 @@
 # Solution -4
 num = input ("Please insert positive Integer: ")
 intnum = int(num)
-while intnum != 1:
+while intnum != 0:
     print (intnum)
     if intnum % 2 == 0:
         intnum = int(intnum / 2)
+    elif intnum ==1:
+        intnum = 0
     else:
         intnum = (intnum * 3) + 1
