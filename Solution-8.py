fd0a200b9ff129aa9cb4e6e5afd15de71c53a34a
Lisa Murray <lisa.murray1605@gmail.com>
2019-03-24 14:25:52 +0000

    Solution-8 to change from 24 hour to 12 hour format

--- Solution-8.py
+++ Solution-8.py
@@ -4,10 +4,9 @@ import datetime
 today = datetime.datetime.today()
 
 dayOfMonth = int(today.strftime("%d"))
-hour = int(today.strftime("%H"))
 
 daySuf = "th"
-timeSuf = "pm"
+
 if dayOfMonth == 3 or dayOfMonth == 23:
     daySuf = "rd"
 elif dayOfMonth == 2 or dayOfMonth == 22:
@@ -15,11 +14,8 @@ elif dayOfMonth == 2 or dayOfMonth == 22:
 elif dayOfMonth == 1 or dayOfMonth == 21 or dayOfMonth == 31:
     daySuf = "st"
 
-if hour < 13:
-    timeSuf = "am"
 
-
-print(today.strftime("%A, %B %d" + daySuf + " %Y at %H:%M" + timeSuf))
+print(today.strftime("%A, %B %d" + daySuf + " %Y at %#I:%M %p"))
 
 
 
