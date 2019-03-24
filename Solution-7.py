91a24b3bcb9cece063942dacc18e254aba114c75
Lisa Murray <lisa.murray1605@gmail.com>
2019-03-24 10:31:37 +0000

    Solutiion 7 updated to round numbers and use newtowns square root

--- Solution-7.py
+++ Solution-7.py
@@ -1,5 +1,10 @@
-num = float (input("Please enter a positive number: "))
+# The number to calculate the square root of:
+sqroot = float (input("Please enter a positive number: "))
+estimate = 6.0
 
-sqroot = num**(1/2)
+while abs((estimate *estimate) - sqroot) > 0.1:
+    estimate -=((estimate * estimate) - sqroot) / (2* estimate)
 
-print (sqroot)
+ans = str(round(estimate, 1))
+
+print (f"The square root of {sqroot} is approx. {ans}.")
